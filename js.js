bc48371d94e3f8fbddf7e33726715f3cfa246a7f
R3dBlck <laura34amiot@gmail.com>
2021-11-17 20:57:02 +0100

    dfghjkl

---
+++ js.js
@@ -0,0 +1,10 @@
+document.getElementById("cadre_projet_1").checked;
+if (document.getElementById("cadre_projet_1").checked === true) {
+ alert("Salut moi c'est laura");
+
+}
+document.getElementById("emergance_sponso_1").checked;
+if (document.getElementById("emergance_sponso_1").checked === true) {
+ alert("Salut moi c'est laura");
+
+}
