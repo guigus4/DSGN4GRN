50ad789bee5ee1f27501de0dcd7f84d9bb4a22fa
R3dBlck <laura34amiot@gmail.com>
2021-11-18 14:04:25 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,10 +1,15 @@
 document.getElementById("cadre_projet_1").checked;
 if (document.getElementById("cadre_projet_1").checked === true) {
  alert("Salut moi c'est laura");
-
 }
-document.getElementById("emergance_sponso_1").checked;
-if (document.getElementById("emergance_sponso_1").checked === true) {
- alert("Salut moi c'est laura");
 
+
+
+
+
+var btnPopup = document.getElementById('btnPopup');
+var overlay = document.getElementById('overlay');
+btnPopup.addEventListener('click',openMoadl);
+function openMoadl() {
+overlay.style.display='block';
 }
